c7cbb8926c25e69a0136d509635660adf96badce
Kaw Mont <kawmont.tech@gmail.com>
2023-06-08 12:41:31 -0300

    Primeiro commit do Projeto Vollmed

---
+++ api/src/main/java/med/voll/api/domain/paciente/DadosAtualizacaoPaciente.java
@@ -0,0 +1,12 @@
+package med.voll.api.domain.paciente;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.endereco.DadosEndereco;
+
+public record DadosAtualizacaoPaciente(
+        Long id,
+        String nome,
+        String telefone,
+        @Valid DadosEndereco endereco
+) {
+}
